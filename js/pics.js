0a301ac6200be8e241aec4dc4f2d5c4aee5580db
coreyCor <coreycor904@outlook.com>
2017-06-22 11:29:01 -0700

    some revisions

---
+++ js/pics.js
@@ -0,0 +1,62 @@
+	 var pics = [
+  {
+	
+
+  pic: "img/10.jpg",
+  url: "img/10.jpg"
+
+  },
+
+  { 
+
+  pic: "img/2.jpg",
+  url: "img/2.jpg"
+
+  },
+
+  { 
+
+  pic: "img/3.jpg",
+  url: "img/3.jpg"
+ 
+  },
+
+
+  { 
+
+  pic: "img/5.jpg",
+  url: "img/5.jpg"
+  },
+
+  { 
+
+  pic: "img/15.jpg",
+  url: "img/15.jpg"
+
+  },
+
+  {  
+
+  pic: "img/7.jpg",
+  url: "img/7.jpg"
+
+  },
+
+  {
+
+  pic: "img/8.jpg",
+  url: "img/8.jpg"
+  },
+  {
+  pic: "img/9.jpg",
+  url:  "img/9.jpg"
+  },
+
+  {
+  pic: "img/14.jpg",
+  url: "img/14.jpg"
+
+  },
+
+
+     ];

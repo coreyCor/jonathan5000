e3f518a84aa28ca29195705358b16f0aa4c9d9bc
coreyCor <coreycor904@outlook.com>
2017-06-22 11:34:03 -0700

    added materialize and mat box for media

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //<p class="j316">Stay Tuned For More . . .
 //<img src="img/johnbro.jpg" class="thumbnail" style="height: 200px">Coming Soon . . .</p>
 for(var i = 0; i < pics.length; ++i){
-  $("#pic-area").append("<a href= "+pics[i].url+" ><div class='col-md-4 col-xs-6'>\
-	<img src="+ pics[i].pic +" class='thumbnail'></div></a>")};
+  $("#pic-area").append("<div class='col-md-4 col-xs-6'>\
+	<img src="+ pics[i].pic +" class='thumbnail materialboxed'></div>")};
